42fe20153c08ef5fde89efd6756cb01359ee1872
ozavalina <zavalina.olga11@gmail.com>
2023-05-03 23:08:51 +0300

    Hello from Olga

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,14 +1,9 @@
-import io.restassured.RestAssured;
-import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
 
   @Test
   public void testHelloWorldTest() {
-    Response response = RestAssured
-            .get("https://playground.learnqa.ru/api/hello")
-            .andReturn();
-    response.prettyPrint();
+    System.out.println("Hello from Olga");
   }
 }
